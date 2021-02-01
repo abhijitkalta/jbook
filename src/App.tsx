5f0ce799e5fd640a40a5bf07e8e049b5648bf03f
Abhijit Kalta <abhijitkalta@gmail.com>
2021-02-01 10:36:17 +0530

    Add esbuild Plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App: React.FC = () => {
   const [input, setInput] = useState("");
@@ -22,11 +23,13 @@ const App: React.FC = () => {
     if (!ref.current) {
       return;
     }
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   };
 
   return (
