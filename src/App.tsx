35fbee2b2d5be4ca29fb3540021b9af253809080
Abhijit Kalta <abhijitkalta@gmail.com>
2021-02-04 02:19:38 +0530

    Add text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import "./App.css";
 import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <TextEditor />
       <CodeCell />
     </div>
   );
---
+++ src/components/text-editor.tsx
@@ -0,0 +1,43 @@
+import "./text-editor.css";
+import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor className="text-editor" />
+      </div>
+    );
+  }
+  return (
+    <div
+      onClick={() => {
+        setEditing(true);
+      }}
+    >
+      <MDEditor.Markdown className="text-editor" source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;
